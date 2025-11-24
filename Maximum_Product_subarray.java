cb4d05f8ce5b4585e88d668f3741bc6e3367f29b
sanjuz7 <sanjayk91940@gmail.com>
2025-11-24 22:21:22 +0530

    first commit

---
+++ Maximum_Product_subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int prefix = 1;
+        int suffix=1;
+        int max1 =Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(prefix ==0){
+                prefix=1;
+            }
+            if(suffix==0){
+                suffix=1;
+            }
+            prefix=prefix*nums[i];
+            suffix=suffix*nums[nums.length-i-1];
+
+            max1=Math.max(Math.max(prefix,suffix),max1);
+        }
+
+        return max1;
+    }
+}
