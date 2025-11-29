86b2b72c6863de06141fb50bd4717997c10f4daa
sanjuz7 <sanjayk91940@gmail.com>
2025-11-29 20:23:58 +0530

    String problem -389

---
+++ String/Find_the_Difference.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        if(s.length()==0) return t.charAt(0);
+        Map <Character,Integer> map = new HashMap<>();
+        for(char c :s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);               
+        }
+        for(char c :t.toCharArray()){
+            if(!map.containsKey(c)||map.get(c)==0){
+                return c;
+            }
+            map.put(c,map.get(c)-1);
+        }
+        return ' ';
+    }
+}
